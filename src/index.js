615e0d00d1e2fd51197e328dcc48c137e4eeb4a1
github username <github email address>
2020-07-31 15:18:15 -0400

    add dependancies to top of file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redus-thunk'
+import { Provider } from 'react-redux'
+
 import './index.css';
 import App from './App';
+
+//set up store
 
 ReactDOM.render(
   <React.StrictMode>
