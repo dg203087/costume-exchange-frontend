9519759ad4dc1c522259d82b1e6f8fea76ee08c1
github username <github email address>
2020-08-03 15:55:24 -0400

    created store and connected devtools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redus-thunk'
 import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
 
-//set up store
+//compose allows you to chain together middleware
+//allows access to devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+//store - where data will be stored globally - send action object to reducer
+//reducers will tell us what we do with our store based on actions - return new version
+// first element is the reducer 
+// second is any sort of middleware - thunk
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+//provider so store is global to all components creates
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
