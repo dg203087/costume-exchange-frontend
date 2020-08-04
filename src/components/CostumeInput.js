291ba9a5df7b8412f6510bf6f0e86fb96a8d52b5
github username <github email address>
2020-08-03 22:32:31 -0400

    created form component and connected to container

---
+++ src/components/CostumeInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//class because there is local state holding form values
+class CostumeInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Costume Input
+            </div>
+        )
+    }
+}
+
+export default CostumeInput
