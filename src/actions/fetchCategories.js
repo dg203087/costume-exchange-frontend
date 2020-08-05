597967be70aaefa09437a35ef3a49a2434bea4d8
github username <github email address>
2020-08-04 22:48:43 -0400

    add action to fetch cateories for costume form

---
+++ src/actions/fetchCategories.js
@@ -0,0 +1,10 @@
+export function fetchCategories() {
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/categories')
+        .then(response => response.json())
+        .then(categories => dispatch({
+            type: 'FETCH_CATEGORIES',
+            payload: categories
+        }))
+    }
+}
