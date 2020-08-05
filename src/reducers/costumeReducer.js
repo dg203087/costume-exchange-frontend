0f07e3b4a9af4d2ae52ba165cb925402a9a38416
github username <github email address>
2020-08-04 22:52:52 -0400

    add ADD_COSTUME action

--- src/reducers/costumeReducer.js
+++ src/reducers/costumeReducer.js
@@ -7,6 +7,8 @@ export default function costumeReducer(state = {costumes: []}, action) {
     switch (action.type) {
         case 'FETCH_COSTUMES':
             return {costumes: action.payload} //new state
+        case 'ADD_COSTUME':
+            return {...state, costumes: [...state.costumes, action.payload]} //returns all state
         default: 
             return state
     }
