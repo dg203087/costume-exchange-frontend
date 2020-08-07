b8ee6322c37ac319b143269d138df3b25bd99835
github username <github email address>
2020-08-06 23:46:39 -0400

    create component with navbar and header

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap';
+
+const NavigationBar = () => {
+  return (
+    <div>
+    <Navbar collapseOnSelect expand="lg" bg="danger" variant="light">
+        <Navbar.Brand href="/costumes">
+          <img alt="img" src="/hero.png" width="50" height="50" className="d-inline-block align-top"/>
+          {' '}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/costumes/new">Post Your Costumes</Nav.Link>
+          <Nav.Link href="/costumes">Costume List</Nav.Link>
+          <Nav.Link href="/categories">Costumes By Category</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <img src="/header2.png" width="100%" height="100%" />
+    <br></br><br></br>
+  </div>
+  )
+}
+
+export default NavigationBar
+
