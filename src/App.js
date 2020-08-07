0604587c0ea1add9c51f0d2f2d15f61db0c9e93c
github username <github email address>
2020-08-06 23:47:41 -0400

    add Navbar rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-// import { connect } from 'react-redux'
 import CostumesContainer from './containers/CostumesContainer'
-// import { fetchCostumes } from './actions/fetchCostumes'
-import './App.css';
+import CategoriesContainer from './containers/CategoriesContainer';
+import NavigationBar from './components/NavigationBar'
 
 class App extends React.Component {
-
 
   render() {
     return (
       <div className="App">
+        <NavigationBar />
+        <CategoriesContainer />
         <CostumesContainer />
       </div>
     )
   }
 }
 
-
-//update store directly from component - give access to this.props.fetchCostumes
-//essentially calling store.dispatch on fetchCostumes()
 export default (App)
