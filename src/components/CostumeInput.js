dd72f5599d3e35960c23795a822d7dcc2b595b04
github username <github email address>
2020-08-13 00:05:29 -0400

    add handleImageChange function and removed value= for photo element

--- src/components/CostumeInput.js
+++ src/components/CostumeInput.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addCostume } from '../actions/addCostume'
+import { Form, Button, Col } from 'react-bootstrap'
+// import { Redirect } from 'react-router-dom';
 
 //class because there is local state holding form values
 class CostumeInput extends React.Component {
     //local state in a controlled form - not in redux store (would use store if going to edit a form to reuse form)
-    state = {
-        title: '',
-        price: '', 
-        location: '',
-        owner_name: '', 
-        owner_email: '', 
-        description: '',
-        photos: null,
-        category_id: 1
+    
+    constructor(props) {
+        super()
+        this.state = {
+            title: '',
+            price: '', 
+            location: '',
+            owner_name: '', 
+            owner_email: '', 
+            description: '',
+            photo: '',
+            categories: [
+                {id: 1, name: 'Film'}
+            ]
+        }
     }
 
     // handleChange automatically passes event to first parameter
@@ -25,10 +33,21 @@ class CostumeInput extends React.Component {
         })
     }
 
+    handleImageChange = (event) => {
+        if (event.target.value) {
+            let path = event.target.value
+            let newPath = path.replace(/^C:\\fakepath\\/, "")
+            this.setState({ 
+                photo: newPath
+            })
+        }
+    }
+
     handleSubmit = (event) => {
         event.preventDefault() //prevent from re-render
         this.props.addCostume(this.state) //needs argument to pass into action
         // setState is asynch - doesn't matter which order it is placed in
+        
         this.setState({
             title: '',
             price: '', 
@@ -36,50 +55,68 @@ class CostumeInput extends React.Component {
             owner_name: '', 
             owner_email: '', 
             description: '',
-            photos: null,
-            category_id: 1
+            photo: '',
+            categories: [
+                {id: 1, name: 'Film'}
+            ]
         })
     }
 
-    // render - only in class component
-    render(props) {
+    render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Costume Listing Title: </label>
-                    <input name="title" type='text' value={this.state.title} onChange={this.handleChange} placeholder="e.g., Batman Costume, medieval corset"/><br></br>
-                    
-                    <label>Price to Borrow/Sell: </label>
-                    <input name="price" type='text' value={this.state.price} onChange={this.handleChange} placeholder="e.g., $25, free to borrow"/><br></br>
+                <center><h2>Add A Costume Listing</h2></center>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group as={Col} >
+                        <Form.Label>Costume Listing Title: </Form.Label>
+                        <Form.Control name="title" type='text' value={this.state.title} onChange={this.handleChange} placeholder="e.g., Batman Costume, medieval corset"/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} >
+                        <Form.Label>Price to Borrow/Sell: </Form.Label>
+                        <Form.Control name="price" type='text' value={this.state.price} onChange={this.handleChange} placeholder="e.g., $25, free to borrow"/>
+                    </Form.Group>
                    
-                    {/* <label>Select a Category: </label>
-                    <select name='category_id' value={this.props.category_id} onChange={this.handleChange} >
+                    <Form.Group as={Col} >
+                    <Form.Label>Select a Category: </Form.Label>
+                    <Form.Control as="select" name='category_id' onChange={this.handleChange}>
                         {this.props.categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)}
-                    </select><br></br> */}
+                    </Form.Control>
+                    </Form.Group>
                     
-                    <label>Location/Neighborhood: </label>
-                    <input name="location" type='text' value={this.state.location} onChange={this.handleChange} placeholder="e.g., Midtown NYC, Astoria, Queens"/><br></br>
-                   
-                    <label>Owner Name: </label>
-                    <input name="owner_name" type='text' value={this.state.owner_name} onChange={this.handleChange} /><br></br>
-                   
-                    <label>Owner E-mail: </label>
-                    <input name="owner_email" type='text'value={this.state.owner_email} onChange={this.handleChange} /><br></br>
+                    <Form.Group as={Col} >
+                        <Form.Label>Location/Neighborhood: </Form.Label>
+                        <Form.Control name="location" type='text' value={this.state.location} onChange={this.handleChange} placeholder="e.g., Midtown NYC, Astoria, Queens"/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} >
+                        <Form.Label>Owner Name: </Form.Label>
+                        <Form.Control name="owner_name" type='text' value={this.state.owner_name} onChange={this.handleChange} />
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Label>Owner E-mail: </Form.Label>
+                        <Form.Control name="owner_email" type='text'value={this.state.owner_email} onChange={this.handleChange} />
+                    </Form.Group>
+
+                    <Form.Group as={Col} >
+                        <Form.Label>Costume Description: </Form.Label>
+                        <Form.Control as="textarea" name="description" rows='4' cols='50' value={this.state.description} onChange={this.handleChange} placeholder="e.g., Black catsuit, fits size small/medium. Available for trades/borrows only."/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} >
+                        <Form.File name="photo" accept="image/png, image/jpeg" label="Upload Costume Photo:" onChange={this.handleImageChange}/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} >
+                    <Button type="submit" variant="danger">Post Costume</Button><br></br>
+                    </Form.Group>
                     
-                    <label>Costume Description: </label>
-                    <textarea name="description" rows='4' cols='50' value={this.state.description} onChange={this.handleChange} placeholder="e.g., Black catsuit, fits size small/medium. Available for trades/borrows only."/><br></br>
-                    <input type="submit"/><br></br>
-                </form>
+                </Form>
             </div>
         )
     }
 }
-
-// function mapStateToProps(state) {
-//     return {
-//       categories: state.categoryReducer.category
-//     }
-// }  
 
 //responsible to adding data to store - don't need mapStateToProps
 //without thunk we would have to call mapDispatch, but now we have access to it in the action
