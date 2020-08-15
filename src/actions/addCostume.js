3058a195153b987bba5422120635f3fc33180f74
github username <github email address>
2020-08-15 14:04:12 -0400

    attempt active storage, fail, leave notes for later

--- src/actions/addCostume.js
+++ src/actions/addCostume.js
@@ -1,6 +1,9 @@
+// import { DirectUpload } from 'activestorage'
+
 export const addCostume = (data) => {
     //using thunk - dispatch function can be called within action
     //needs to be sent to back end
+    
     return (dispatch) => {
         fetch('http://localhost:3000/api/v1/costumes', {
             headers: {
@@ -16,5 +19,20 @@ export const addCostume = (data) => {
             type: 'ADD_COSTUME', 
             payload: costume
         }))
+        // .then(data => uploadFile(photo, data))
     }
+
 }
+
+// const uploadFile = (file, costume) => {
+    
+//     const upload = new DirectUpload(file, 'http://localhost:3000/api/v1/rails/active_storage/direct_uploads')
+//     debugger
+//     upload.create((error, blob) => {
+//         if (error) {
+//             console.log(error)
+//         } else {
+//             console.log('no error')
+//         }
+//     })
+// }
