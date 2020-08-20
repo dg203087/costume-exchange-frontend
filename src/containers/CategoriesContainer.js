f9c1e6fe658f40711abf210b7371d7505cbfee43
Deanna Kelley <deanna.georges@gmail.com>
2020-08-19 21:03:06 -0400

    add mapDispatchToProps

--- src/containers/CategoriesContainer.js
+++ src/containers/CategoriesContainer.js
@@ -1,24 +1,53 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 import {fetchCategories} from '../actions/fetchCategories'
-import Categories from '../components/Categories'
-import Category from '../components/Category'
+import {filterCostume} from '../actions/filterCostume'
+import Costumes from '../components/Costumes'
+// import Categories from '../components/Categories'
 
-class CategoriesContainer extends React.Component {
+class CategoriesContainer extends React.Component { 
     
+    constructor(props) {
+        super(props)
+        this.state = {
+            costumes: [this.props.costumes]
+        }
+    }
+
     componentDidMount(){
         this.props.fetchCategories()
     }
 
+    filter = (event) => {
+        event.preventDefault()
+        const categoryID = event.target.value
+        const costumeProps = this.props.costumes
+        // const filteredCostumes = this.props.costumes.filter(costume => [costume.category.id] == categoryID)
+        // console.log(filteredCostumes)
+        this.props.filterCostume(categoryID, costumeProps)
+        // this.setState({
+        //     costumes: filteredCostumes
+        // }
+    }
+
+
     render() {
         return (
             <div>
-                <Categories categories={this.props.categories} />
+                <div className="all_categories">
+                
+                    <h5>Filter Costumes by Category</h5>
+                    {this.props.categories.map(category => 
+                        <Button variant="danger" key={category.id} value={category.id} style={{margin: '10px'}} onClick={this.filter}>{category.name}</Button>
+                    )}
+                        <Button to='/costumes' variant="danger" style={{margin: '10px'}}>All Costumes</Button>
+                </div>
                 <br></br>
-                <Route path='/categories/:id' render={(routerProps) => <Category {...routerProps} categories={this.props.categories}/> } />
-                <Route path='/categories' render={(routerProps) => <Categories {...routerProps} categories={this.props.categories}/> } />
+                <div className='all_costumes'> 
+                    <Costumes costumes={this.props.costumes}/>   
+                </div>
             </div>
         )
     }
@@ -30,5 +59,11 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, {fetchCategories})(CategoriesContainer)
-// export default CategoriesContainer
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchCategories, 
+        filterCostume: (categoryID, costumeProps) => dispatch(filterCostume(categoryID, costumeProps))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoriesContainer)
