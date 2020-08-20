582e70ac07f9516783310be511fb026014e2ae98
Deanna Kelley <deanna.georges@gmail.com>
2020-08-19 21:06:15 -0400

    removed notes

--- src/reducers/costumeReducer.js
+++ src/reducers/costumeReducer.js
@@ -1,13 +1,11 @@
-//action comes from action creator
-//function that takes in
-//1. state (object) usually previous state and 
-//2. action
 export default function costumeReducer(state = {costumes: []}, action) {
     switch (action.type) {
         case 'FETCH_COSTUMES':
             return {costumes: action.payload} //new state
         case 'ADD_COSTUME':
             return {...state, costumes: [...state.costumes, action.payload]} //returns all state
+        case  'DELETE_COSTUME':
+            return {costumes: state.costumes.filter(costume => costume.id !== costume.id)}
         default: 
             return state
     }
