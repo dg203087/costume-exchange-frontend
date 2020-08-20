812442c34d67d40aa018e872231ed62baff15c23
Deanna Kelley <deanna.georges@gmail.com>
2020-08-19 21:00:55 -0400

    add basic site footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () =>{
+  return (
+    <div className='footer'>
+      <br/>
+    &nbsp;&nbsp;  © 2020 NYC Costume Exchange, Inc. 
+      <br/>
+    </div>
+  )
+}
+
+export default Footer
