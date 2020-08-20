9c7b782be5f0b72ecca27ef872477374d53e078f
Deanna Kelley <deanna.georges@gmail.com>
2020-08-19 21:00:23 -0400

    add card coloring and style

--- src/components/Costumes.js
+++ src/components/Costumes.js
@@ -1,23 +1,17 @@
 //functional - renders a list of costumes - don't need render
-//have to pass the props in as an argument
-//refactor with deconstruction
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card, CardColumns } from 'react-bootstrap'
-// import CategoriesContainer from '../containers/CategoriesContainer'
 
 const Costumes = (props) => {
-
-    return (
+    return (  
         <div>
-            {/* <CategoriesContainer /> */}
-            <center><h2>Available Costumes</h2></center>
-                        
+            <center><h2>Available Costumes</h2></center>           
             <CardColumns>
+                
             {props.costumes.map(costume => 
-                <Card key={costume.id} border="danger" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={costume.photo} />
+                <Card key={costume.id} border='danger' style={{ width: '18rem' }}>
+                <Card.Img variant='top' src={costume.photo} />
                     <Card.Body>
                         <Link to={`/costumes/${costume.id}`} style={{ color: '#E74C3C' }} >
                         <Card.Title>{costume.title} - {costume.price}</Card.Title></Link>
